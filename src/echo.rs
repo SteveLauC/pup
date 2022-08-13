a19aecb4e1764dd5b478d2956f94db21cac9cefd
SteveLauC <stevelauc@outlook.com>
2022-08-13 11:01:43 +0800

    fix: issue2

--- src/echo.rs
+++ src/echo.rs
@@ -1,25 +1,25 @@
 //! echo.rs: offers functionality of disabling and enabling ECHO on tty
 
-use termios::{tcsetattr, Termios, ECHO, TCSANOW};
+use nix::sys::termios::{tcgetattr, tcsetattr, LocalFlags, SetArg, Termios};
 
 /// purpose: disable echo on tty configuration
 ///
 /// action: unset ECHO bit of termios.c_lflag
 pub fn echo_off() {
-    let mut t = Termios::from_fd(0).unwrap();
+    let mut t: Termios = tcgetattr(0).unwrap();
 
-    t.c_lflag &= !ECHO;
+    t.local_flags.remove(LocalFlags::ECHO);
 
-    tcsetattr(0, TCSANOW, &t).unwrap();
+    tcsetattr(0, SetArg::TCSANOW, &t).unwrap();
 }
 
 /// purpose: enable echo on tty configuration
 ///
 /// action: set ECHO bit of termios.c_lflag
 pub fn echo_on() {
-    let mut t = Termios::from_fd(0).unwrap();
+    let mut t: Termios = tcgetattr(0).unwrap();
 
-    t.c_lflag |= ECHO;
+    t.local_flags.insert(LocalFlags::ECHO);
 
-    tcsetattr(0, TCSANOW, &t).unwrap();
+    tcsetattr(0, SetArg::TCSANOW, &t).unwrap();
 }
